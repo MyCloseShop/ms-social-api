f48e668338d0dac6bb73df3a61fc500dd1d1a1d2
amara_s <amara_s@etna-alternance.net>
2025-05-21 10:50:33 +0200

    feat(ms-social-api): init project

--- src/main/resources/db/migration/base_db.sql
+++ src/main/resources/db/migration/base_db.sql
@@ -1,2 +1,3 @@
 -- Create database and use it
-CREATE DATABASE IF NOT EXISTS `db-ms-quote` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE
+DATABASE IF NOT EXISTS `db-ms-social` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
