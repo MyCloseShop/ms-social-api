703e8340eb57dee079dee2cc961fa0b1e8f77212
amara_s <amara_s@etna-alternance.net>
2025-06-17 21:55:31 +0200

    feat(ms-social-api): update post and comment table schemas to use TEXT for descriptions and content

--- src/main/resources/db/migration/V1__create_post_table.sql
+++ src/main/resources/db/migration/V1__create_post_table.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS `post`
 (
     post_id  BINARY(16) NOT NULL,
     nom VARCHAR(50) NOT NULL,
-    description VARCHAR(255) NOT NULL,
+    description TEXT NULL,
     media VARCHAR(255),
     shop_id BINARY(16) NOT NULL,
     created_at    datetime DEFAULT NOW() NULL,
--- src/main/resources/db/migration/V2__create_comment_table.sql
+++ src/main/resources/db/migration/V2__create_comment_table.sql
@@ -1,8 +1,7 @@
 CREATE TABLE IF NOT EXISTS `comment`
 (
     comment_id  BINARY(16) NOT NULL,
-    content VARCHAR(255) NOT NULL,
-    media VARCHAR(255),
+    content TEXT NOT NULL,
     post_id BINARY(16) NOT NULL,
     user_id BINARY(16) NOT NULL,
     created_at    datetime DEFAULT NOW() NULL,
