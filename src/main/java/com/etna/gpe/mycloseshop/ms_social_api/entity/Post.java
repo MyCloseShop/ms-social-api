dec745940b86bd4e05b5c31347564e9157924919
amara_s <amara_s@etna-alternance.net>
2025-07-17 11:48:24 +0200

    refactor(entity): update imports in Comment and Post classes for clarity

--- src/main/java/com/etna/gpe/mycloseshop/ms_social_api/entity/Post.java
+++ src/main/java/com/etna/gpe/mycloseshop/ms_social_api/entity/Post.java
@@ -1,6 +1,12 @@
 package com.etna.gpe.mycloseshop.ms_social_api.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 import java.time.LocalDateTime;
 import java.util.UUID;
 
